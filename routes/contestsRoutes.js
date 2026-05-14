3b95aa44aa7338e40075689d0049609cbf133689
Md.Arif Ahmed <mohammad.arif.ahmed.08@gmail.com>
2026-05-14 20:20:04 +0600

    implement contest crud and search apis

--- routes/contestsRoutes.js
+++ routes/contestsRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 
 const router = express.Router();
 
@@ -19,7 +20,6 @@ const contestsRoutes = (
 
         const contest = req.body;
 
-        // default values
         contest.status = "pending";
 
         contest.participantsCount = 0;
@@ -66,6 +66,192 @@ const contestsRoutes = (
     }
 
   });
+
+  // search contests
+  router.get("/search/type", async (req, res) => {
+
+    try {
+
+      const type = req.query.type;
+
+      const query = {
+        contestType: {
+          $regex: type,
+          $options: "i",
+        },
+
+        status: "approved",
+      };
+
+      const result = await contestsCollection
+        .find(query)
+        .toArray();
+
+      res.send(result);
+
+    } catch (error) {
+
+      res.status(500).send({
+        message: error.message,
+      });
+
+    }
+
+  });
+
+  // popular contests
+  router.get("/popular/contests", async (req, res) => {
+
+    try {
+
+      const query = {
+        status: "approved",
+      };
+
+      const result = await contestsCollection
+        .find(query)
+        .sort({ participantsCount: -1 })
+        .limit(5)
+        .toArray();
+
+      res.send(result);
+
+    } catch (error) {
+
+      res.status(500).send({
+        message: error.message,
+      });
+
+    }
+
+  });
+
+  // my created contests
+  router.get(
+    "/creator/my-contests",
+    verifyJWT,
+    verifyCreator,
+    async (req, res) => {
+
+      try {
+
+        const email = req.query.email;
+
+        const query = {
+          creatorEmail: email,
+        };
+
+        const result = await contestsCollection
+          .find(query)
+          .sort({ createdAt: -1 })
+          .toArray();
+
+        res.send(result);
+
+      } catch (error) {
+
+        res.status(500).send({
+          message: error.message,
+        });
+
+      }
+
+    }
+  );
+
+  // get single contest
+  router.get("/:id", async (req, res) => {
+
+    try {
+
+      const id = req.params.id;
+
+      const query = {
+        _id: new ObjectId(id),
+      };
+
+      const result = await contestsCollection.findOne(query);
+
+      res.send(result);
+
+    } catch (error) {
+
+      res.status(500).send({
+        message: error.message,
+      });
+
+    }
+
+  });
+
+  // delete contest
+  router.delete(
+    "/:id",
+    verifyJWT,
+    verifyCreator,
+    async (req, res) => {
+
+      try {
+
+        const id = req.params.id;
+
+        const query = {
+          _id: new ObjectId(id),
+        };
+
+        const result = await contestsCollection.deleteOne(query);
+
+        res.send(result);
+
+      } catch (error) {
+
+        res.status(500).send({
+          message: error.message,
+        });
+
+      }
+
+    }
+  );
+
+  // update contest
+  router.put(
+    "/:id",
+    verifyJWT,
+    verifyCreator,
+    async (req, res) => {
+
+      try {
+
+        const id = req.params.id;
+
+        const updatedContest = req.body;
+
+        const query = {
+          _id: new ObjectId(id),
+        };
+
+        const updateDoc = {
+          $set: updatedContest,
+        };
+
+        const result = await contestsCollection.updateOne(
+          query,
+          updateDoc
+        );
+
+        res.send(result);
+
+      } catch (error) {
+
+        res.status(500).send({
+          message: error.message,
+        });
+
+      }
+
+    }
+  );
 
   return router;
 };
