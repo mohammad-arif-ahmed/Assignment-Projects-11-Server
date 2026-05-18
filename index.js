157412f2d8d0fdfaffb40ed3ec085cd3a264387b
Md.Arif Ahmed <mohammad.arif.ahmed.08@gmail.com>
2026-05-18 22:20:52 +0600

    add netlify url link

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",                 
+      "https://contesthub01.netlify.app"       
+    ],
     credentials: true,
   })
 );
